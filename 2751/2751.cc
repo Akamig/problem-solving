15ebc5a47b66c73a6a3a109ea7f732fc0f3cfcad
Akamig <akamig@posteo.net>
2020-06-12 22:57:28 +0900

    Used int_fast

--- 2751/2751.cc
+++ 2751/2751.cc
@@ -1,32 +1,31 @@
 #include <iostream>
 using namespace std;
+int_fast32_t *arr;
 
-void heapify(int_fast32_t array[], int index, int n) {
-  int l;
-  if ((l = 2 * index + 1) >= n){
+void heapify(int_fast32_t array[], int_fast32_t index, int_fast32_t n) {
+  int_fast32_t l;
+  if ((l = 2 * index + 1) >= n) {
     return;
   }
-  int r = l+1;
-  
-  int largest = index;
-  if (array[l] > array[largest]){
+  int_fast32_t r = l + 1;
+  int_fast32_t largest = index;
+  if (array[l] > array[largest]) {
     largest = l;
   }
   if (r < n && array[r] > array[largest]) {
     largest = r;
   }
-  if(largest != index){
+  if (largest != index) {
     swap(array[index], array[largest]);
     heapify(array, largest, n);
   }
 }
 
-void heapsort(int_fast32_t array[], int n) {
-  for (int i = n / 2 - 1; i >= 0; i--)
+void heapsort(int_fast32_t array[], int_fast32_t n) {
+  for (int_fast32_t i = n / 2 - 1; i >= 0; i--)
     heapify(array, i, n);
 
-  for (int i = n - 1; i >= 1; i--)
-  {
+  for (int_fast32_t i = n - 1; i >= 1; i--) {
     swap(array[0], array[i]);
     heapify(array, 0, i);
   }
@@ -36,15 +35,15 @@ int main(void) {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  
-  int n;
+
+  int_fast32_t n;
   cin >> n;
-  int_fast32_t arr[n];
-  for (int i = 0; i < n; i++) {
+  arr = new int_fast32_t[n];
+  for (int_fast32_t i = 0; i < n; i++) {
     cin >> arr[i];
   };
   heapsort(arr, n);
-  for (int i = 0; i < n; i++) {
+  for (int_fast32_t i = 0; i < n; i++) {
     cout << arr[i] << '\n';
   };
   return 0;
